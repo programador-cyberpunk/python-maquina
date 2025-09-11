037a3ca095f7812fd5c5b67d3ed9f284f9c5955c
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2025-09-11 17:07:23 -0300

    Add files via upload

---
+++ aprendendo_pandas.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+serie_vazia =  pd.Series(dtype='object')
+print("Tem nada nessa porra")
+print(serie_vazia)
+
+print("-" * 30)
+
+dados_array = np.array =(["N",'a','i','l','b','o','m','b'])
+serie_letras = pd.Series(dados_array)
+print(serie_letras)
+
