8104bbc697d93633964d02228affde53f0afc712
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2025-09-30 16:27:30 -0300

    Add files via upload

--- analise_custo_saude.py
+++ analise_custo_saude.py
@@ -74,3 +74,47 @@ sns.heatmap(corr_matrix, annot=True, camp='coolwarm', fmt= '.2f',linewidth=0.5)
 plt.title('Matriz de correlacao')
 plt.show()
 
+#hora de separar as variaves
+X = df.drop('charges', axis=1)
+Y = df['charges']
+
+#chegaram as porra dos teste
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#escalonando os dados
+
+from sklearn.preprocessing import StandardScaler
+
+#vamos isolar as colunas
+numeric_features = ['age', 'bmi', 'children']
+X_train_num = X_train[numeric_features]
+X_teste_num = X_teste[numeric_features]
+
+scaler = StandardScaler()
+X_train_num_scaled = scaler.fit_transform(X_train_num)
+X_teste_num_scaled = scaler.transform(X_teste_num)
+
+#construindo e avaliando a porra da regressão
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+
+#avaliaçao de performance
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
+
+mae = mean_absolute_error(y_test, y_pred)
+mae = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+#amostrando as metrica
+print(f'MAE: {mae:.2f}')
+print(f'MSE: {mse:.2f}')
+print(f'RMSE: {rmse:.2f}')
+print(f'R2: {r2:.2f}')
