b98c3f235647f5f264895807338e700ba385a018
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2025-11-18 23:02:57 -0300

    Add files via upload

---
+++ conjunto/vari-vari-vari.py
@@ -0,0 +1,8 @@
+from IPython.display import Image
+Image(filename='Variancia_vies.png')
+Image(filename='Bagging.png')
+Image(filename='Bagging2.png')
+Image(filename='Boosting.png')
+Image(filename='Boosting2.png')
+Image(filename= 'Stacking.png')
+Image(filename= 'Stacking2.png')
